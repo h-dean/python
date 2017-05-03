7e6f7a6b86d53802429027c82c495411a518969d
h-dean <harry@iaindeandesign.co.uk>
2017-05-03 17:59:25 +0100

    add bubble sorts
    Uploaded two programs which each sort a list of numbers by using a bubble sort algorithm.

---
+++ bubble sort/bubble_lts.py
@@ -0,0 +1,16 @@
+numlist = [2,1,1,4,1,2,4,6,8,9,3,5,7,5]
+
+def bubble_lts(nums):
+    end = len(nums)-1
+    while end >= 0:
+        swapped = -1
+        for i in range(0, end):
+            if nums[i] < nums[i+1]:
+                j = i + 1
+                nums[i], nums[j] = nums[j], nums[i]
+                swapped = i
+        end = swapped
+
+    return nums
+
+print(bubble_lts(numlist))
